9e4a3058ac752cf15e10c41c2a3691b6366b2d27
braacellettt <104008593+GalangPradwita@users.noreply.github.com>
2024-03-13 00:26:48 +0700

    inisialisasi projek

---
+++ tpmodul4_1302223015/tpmodul4_1302223015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul4_1302223015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
